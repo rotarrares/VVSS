55f49270944a381f038cc4e604939a5511b36d0b
rotar <rotarrares95@gmail.com>
2020-03-22 15:56:08 +0200

    Added Second Test Function

--- src/test/java/MaxPointsParticipants/AppTest.java
+++ src/test/java/MaxPointsParticipants/AppTest.java
@@ -2,9 +2,16 @@ package MaxPointsParticipants;
 
 import static org.junit.Assert.assertTrue;
 
+import MaxPointsParticipants.domain.Nota;
 import MaxPointsParticipants.domain.Student;
+import MaxPointsParticipants.domain.Tema;
+import MaxPointsParticipants.repository.NotaXMLRepository;
 import MaxPointsParticipants.repository.StudentXMLRepository;
+import MaxPointsParticipants.repository.TemaXMLRepository;
+import MaxPointsParticipants.service.Service;
+import MaxPointsParticipants.validation.NotaValidator;
 import MaxPointsParticipants.validation.StudentValidator;
+import MaxPointsParticipants.validation.TemaValidator;
 import MaxPointsParticipants.validation.Validator;
 import org.junit.Test;
 
@@ -15,7 +22,7 @@ public class AppTest
 {
     /**
      * Rigorous Test :-)
-     */
+        */
     @Test
     public void shouldAnswerWithTrue()
     {
@@ -24,14 +31,19 @@ public class AppTest
 
     @Test
     public void testAddStudentService() {
-
         Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
         StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "C:\\\\Users\\rotar\\Desktop\\Projects\\vvss\\Lab2\\src\\test\\java\\MaxPointsParticipants\\studenti2.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "C:\\\\Users\\rotar\\Desktop\\Projects\\vvss\\Lab2\\src\\test\\java\\MaxPointsParticipants\\teme2.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "C:\\\\Users\\rotar\\Desktop\\Projects\\vvss\\Lab2\\src\\test\\java\\MaxPointsParticipants\\note2.xml");
 
-        Student student = new Student("1", "nume", 916);
-        fileRepository1.save(student);
-        assertTrue(fileRepository1.findOne(student.getID()).equals(student));
-        fileRepository1.delete(student.getID());
+        Service studentService = new Service(fileRepository1,fileRepository2,fileRepository3);
+        studentService.saveTema("2", "De la mic la mare inaintam tare",31,1);
+        studentService.saveStudent("1", "nume", 916);
+        studentService.saveNota("1","2",32,20,"Formidable! :)");
+
+        assertTrue(studentService.updateStudent("1","Ghencea Mihai",936)==1);
     }
 
     @Test
@@ -44,6 +56,6 @@ public class AppTest
         fileRepository1.save(student);
     assertTrue(fileRepository1.findOne(student.getID()).equals(student));
         fileRepository1.delete(student.getID());
-}
+    }
 
 }
